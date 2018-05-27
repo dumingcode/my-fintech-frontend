448d751878617889123fe5fb854d14db49e6edd3
dumingcode <jake1026@126.com>
2018-05-27 21:26:44 +0800

    添加交易日期

--- src/views/stockIndex/fundFixedInvest.vue
+++ src/views/stockIndex/fundFixedInvest.vue
@@ -4,45 +4,46 @@
 </style>
 
 <template>
-    <div>
-        <Row>
-            <Col span="24" class="padding-left-10 height-100">
-            <Card>
-                <p slot="title" class="card-title">
-                    盈利收益率法估值定投
-                     <Icon type="help" title="定投基准金额 * 定投倍数 * (10/pe)"></Icon>
-                </p>
-                <Table border :data="tableData" :columns="columnsList"></Table>
-            </Card>
-            </Col>
+  <div>
+    <Row>
+      <Col span="24" class="padding-left-10 height-100">
+      
+      <Card>
+        <p slot="title" class="card-title">
+          {{dealDate}}盈利收益率法估值定投
+          <Icon type="help" title="定投基准金额 * 定投倍数 * (10/pe)"></Icon>
+        </p>
+        <Table border :data="tableData" :columns="columnsList"></Table>
+      </Card>
+      </Col>
 
-        </Row>
-        <Row>
-            <Col span="24" class="padding-left-10 height-100">
-            <Card>
-                <p slot="title" class="card-title">
-                    博格公式法估值定投-PE百分位
-                    <Icon type="help" title="定投基准金额 * 定投倍数 * (pe机会值 / 当前pe)"></Icon>
-                </p>
-                <Table border :data="pePosTableData" :columns="columnsList"></Table>
-            </Card>
-            </Col>
+    </Row>
+    <Row>
+      <Col span="24" class="padding-left-10 height-100">
+      <Card>
+        <p slot="title" class="card-title">
+          {{dealDate}}博格公式法估值定投-PE
+          <Icon type="help" title="定投基准金额 * 定投倍数 * (pe机会值 / 当前pe)"></Icon>
+        </p>
+        <Table border :data="pePosTableData" :columns="columnsList"></Table>
+      </Card>
+      </Col>
 
-        </Row>
-         <Row>
-            <Col span="24" class="padding-left-10 height-100">
-            <Card>
-                <p slot="title" class="card-title">
-                    博格公式法估值定投-PB百分位
-                     <Icon type="help" title="定投基准金额 * 定投倍数 * (pb机会值 / 当前pb)"></Icon>
-                </p>
-                <Table border :data="pbPosTableData" :columns="columnsList"></Table>
-            </Card>
-            </Col>
+    </Row>
+    <Row>
+      <Col span="24" class="padding-left-10 height-100">
+      <Card>
+        <p slot="title" class="card-title">
+          {{dealDate}}博格公式法估值定投-PB
+          <Icon type="help" title="定投基准金额 * 定投倍数 * (pb机会值 / 当前pb)"></Icon>
+        </p>
+        <Table border :data="pbPosTableData" :columns="columnsList"></Table>
+      </Card>
+      </Col>
 
-        </Row>
+    </Row>
 
-    </div>
+  </div>
 </template>
 
 <script>
@@ -89,7 +90,7 @@ export default {
           width: 120,
           align: "center"
         },
-          {
+        {
           title: "机会值(20%百分位)",
           key: "chanceVal",
           width: 120,
@@ -108,53 +109,80 @@ export default {
           align: "center"
         }
       ],
+      dealDate:'',
       tableData: [],
-      pePosTableData:[],
-      pbPosTableData:[],      
-      yieldCalcIndex: ['399550','10002', '000015',"000925", "000922", "000300"],
+      pePosTableData: [],
+      pbPosTableData: [],
+      yieldCalcIndex: [
+        "399550",
+        "10002",
+        "000015",
+        "000925",
+        "000922",
+        "000300"
+      ],
       pePosCalcIndex: ["399812", "399971", "000827", "000905"],
-      pbPosCalcIndex: ['399395','399998','399393',"399975", "399986"]
+      pbPosCalcIndex: ["399395", "399998", "399393", "399975", "399986"]
     };
   },
   methods: {
     composeIndexData(stockIndex) {
-      stockIndex["baseMoney"] = 1000;      
+      stockIndex["baseMoney"] = 1000;
       stockIndex["pe"] = stockIndex["pe"].toFixed(2);
       stockIndex["pb"] = stockIndex["pb"].toFixed(2);
-      stockIndex["pe_pos"] = (stockIndex["pe_pos"].toFixed(2) * 100).toFixed(0) + "%";
-      stockIndex["pb_pos"] = (stockIndex["pb_pos"].toFixed(2) * 100).toFixed(0) + "%";
+      stockIndex["pe_pos"] =
+        (stockIndex["pe_pos"].toFixed(2) * 100).toFixed(0) + "%";
+      stockIndex["pb_pos"] =
+        (stockIndex["pb_pos"].toFixed(2) * 100).toFixed(0) + "%";
     },
-
+    async getDealDate() {
+      let lxrDealDate = await axios.get(
+        "/api/indexInvest/queryLxrIndexDealDate.json"
+      );
+      this.dealDate = (lxrDealDate.data.data).substr(0,10)
+    },
     async getIndexData() {
       let lxrIndexAllData = await axios.get(
         "/api/indexInvest/queryLxrIndexAllData.json"
       );
       let indexObj = JSON.parse(lxrIndexAllData.data.data);
       this.yieldCalcIndex.forEach(element => {
-        let stockIndex = indexObj[element]
-        if(!stockIndex) return
-        stockIndex['dtbs'] = 1;
-        stockIndex['chanceVal']=stockIndex.pe_chance_val.toFixed(2)
-        stockIndex["curMoney"] = (10000 * stockIndex["dtbs"] / stockIndex.pe).toFixed(0);
-        this.composeIndexData(stockIndex)
+        let stockIndex = indexObj[element];
+        if (!stockIndex) return;
+        stockIndex["dtbs"] = 1;
+        stockIndex["chanceVal"] = stockIndex.pe_chance_val.toFixed(2);
+        stockIndex["curMoney"] = (
+          10000 *
+          stockIndex["dtbs"] /
+          stockIndex.pe
+        ).toFixed(0);
+        this.composeIndexData(stockIndex);
         this.tableData.push(stockIndex);
       });
       this.pePosCalcIndex.forEach(element => {
-        let stockIndex = indexObj[element]
-        if(!stockIndex) return
-        stockIndex["dtbs"] = 1
-        stockIndex['chanceVal']=stockIndex.pe_chance_val.toFixed(2)
-        stockIndex["curMoney"] = (1000 * stockIndex["dtbs"] * (stockIndex.pe_chance_val/stockIndex.pe)).toFixed(0)
-        this.composeIndexData(stockIndex)
+        let stockIndex = indexObj[element];
+        if (!stockIndex) return;
+        stockIndex["dtbs"] = 1;
+        stockIndex["chanceVal"] = stockIndex.pe_chance_val.toFixed(2);
+        stockIndex["curMoney"] = (
+          1000 *
+          stockIndex["dtbs"] *
+          (stockIndex.pe_chance_val / stockIndex.pe)
+        ).toFixed(0);
+        this.composeIndexData(stockIndex);
         this.pePosTableData.push(stockIndex);
       });
-       this.pbPosCalcIndex.forEach(element => {
-        let stockIndex = indexObj[element]
-        if(!stockIndex) return
-        stockIndex["dtbs"] = 1
-        stockIndex['chanceVal']=stockIndex.pb_chance_val.toFixed(2)
-        stockIndex["curMoney"] = (1000 * stockIndex["dtbs"] * (stockIndex.pb_chance_val/stockIndex.pb)).toFixed(0)
-        this.composeIndexData(stockIndex)
+      this.pbPosCalcIndex.forEach(element => {
+        let stockIndex = indexObj[element];
+        if (!stockIndex) return;
+        stockIndex["dtbs"] = 1;
+        stockIndex["chanceVal"] = stockIndex.pb_chance_val.toFixed(2);
+        stockIndex["curMoney"] = (
+          1000 *
+          stockIndex["dtbs"] *
+          (stockIndex.pb_chance_val / stockIndex.pb)
+        ).toFixed(0);
+        this.composeIndexData(stockIndex);
         this.pbPosTableData.push(stockIndex);
       });
     }
@@ -162,6 +190,7 @@ export default {
   created() {
     // 可在此从服务端获取表格数据
     this.getIndexData();
+    this.getDealDate()
   }
 };
 </script>
