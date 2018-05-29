7ca8422f0b449a0e85ffab86e9ee7ceecc6df39b
dumingcode <jake1026@126.com>
2018-05-29 08:47:26 +0800

    修改website title

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -74,7 +74,7 @@ module.exports = merge(webpackBaseConfig, {
             ]
         }),
         new HtmlWebpackPlugin({
-            title: 'iView admin v' + package.version,
+            title: '滚雪球',
             favicon: './td_icon.ico',
             filename: '../index.html',
             template: '!!ejs-loader!./src/template/index.ejs',
