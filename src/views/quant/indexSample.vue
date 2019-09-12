bf5415d02e76917626065a3a521e4f2ed3f6956e
dumingcode <jake1036@126.com>
2019-09-12 09:41:28 +0800

    指数样本增加表格明细

--- src/views/quant/indexSample.vue
+++ src/views/quant/indexSample.vue
@@ -20,11 +20,19 @@
           </div>
         </Card>
       </Col>
+      <Col span="24" class="padding-left-10 height-200">
+        <Card>
+          <div style="height:600px;width:100%;">
+             <can-edit-table  v-model="tableData" :columns-list="columnsList"></can-edit-table>
+          </div>
+        </Card>
+      </Col>
     </Row>
   </div>
 </template>
 
 <script>
+import canEditTable from '@/views/tables/components/canEditTable.vue'
 import indexSampleChart from './echarts/indexSampleChart.vue'
 import {
     queryUserIndexSampleInfo,
@@ -33,13 +41,34 @@ import {
 
 export default {
     name: 'indexSample',
-    components: { indexSampleChart },
+    components: { indexSampleChart, canEditTable },
     data () {
         return {
             loading: false,
             sData: {}, // 查询统计数据
             sDataOk: false,
-            count: 0
+            count: 0,
+            tableData: [],
+            columnsList: [
+                {
+                    title: '指数名称',
+                    key: 'indexName',
+                    width: 120,
+                    align: 'center'
+                },
+                {
+                    title: '匹配数量',
+                    key: 'num',
+                    width: 120,
+                    align: 'center'
+                },
+                {
+                    title: '匹配个股明细',
+                    key: 'hit_stocks',
+                    width: 160,
+                    align: 'center'
+                }
+            ]
         }
     },
     methods: {
@@ -52,7 +81,8 @@ export default {
                 this.sData = {}
                 this.count = optStocks.split(',').length
                 indexData.forEach(element => {
-                    element['num'] = (element['num'] / this.count).toFixed(2) * 100
+                    element['num'] = (element['num'] / this.count * 100).toFixed(2)
+
                     this.sData[element.index] = element
                 })
                 this.sData['count'] = this.count
